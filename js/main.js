ad4c37a3ab5448622b903d7523f3238781fe261b
bogdan <tokarskii.b@gmail.com>
2017-01-29 00:01:23 +0200

    modal added

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
 jQuery(function ($) {
-    $( function() {
-        var availableTags = [
-            "ActionScript",
-            "AppleScript",
-            "Asp",
-            "BASIC",
-            "C",
-            "C++",
-            "Clojure",
-            "COBOL",
-            "ColdFusion",
-            "Erlang",
-            "Fortran",
-            "Groovy",
-            "Haskell",
-            "Java",
-            "JavaScript",
-            "Lisp",
-            "Perl",
-            "PHP",
-            "Python",
-            "Ruby",
-            "Scala",
-            "Scheme"
-        ];
-        $('.search-field').autocomplete({
-            source: availableTags
-        });
-    } );
+    $('.more-desti-btn').on('click', function () {
+        $('.destinations .more-destinations:first').removeClass('more-destinations');
+        if (!$('.destinations .row').hasClass('more-destinations')) {
+            $('.more-desti-btn').hide()
+        }
+    });
+
+    $('.more-boats-btn').on('click', function () {
+        $('.featured-boats .more-boats:first').removeClass('more-boats');
+        if (!$('.featured-boats .row').hasClass('more-boats')) {
+            $('.more-boats-btn').hide()
+        }
+    });
+
+    $('.modal-destination').on('click', function () {
+        var a = $('h2', this).text(),
+            b = $('p', this).text(),
+            c = $("<p></p>").text(b);
+
+        $('#destination h2').text(a);
+        $('#destination h2').append(c)
+    })
 });
 
