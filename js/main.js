a85676fec352fa0f359f700d9551d51809558786
bogdan <tokarskii.b@gmail.com>
2017-01-28 13:39:58 +0200

    markup done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 jQuery(function ($) {
-
+    $( function() {
+        var availableTags = [
+            "ActionScript",
+            "AppleScript",
+            "Asp",
+            "BASIC",
+            "C",
+            "C++",
+            "Clojure",
+            "COBOL",
+            "ColdFusion",
+            "Erlang",
+            "Fortran",
+            "Groovy",
+            "Haskell",
+            "Java",
+            "JavaScript",
+            "Lisp",
+            "Perl",
+            "PHP",
+            "Python",
+            "Ruby",
+            "Scala",
+            "Scheme"
+        ];
+        $('.search-field').autocomplete({
+            source: availableTags
+        });
+    } );
 });
 
