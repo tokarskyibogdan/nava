86030df415e7a51ab4af2bac58491fb75db4d9ad
bogdan <tokarskii.b@gmail.com>
2017-01-29 02:12:39 +0200

    project done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 jQuery(function ($) {
+
+    $(function() {
+        var destinationTags = [
+            "Aphines",
+            "Africa",
+            "Anapa",
+            "Brasil",
+            "Bangkok",
+            "Boston",
+            "California",
+            "Cuba",
+            "Costarica",
+            "Dallas",
+            "Denver",
+            "Estonia",
+            "Kyiv",
+            "Spain",
+            "Italy",
+            "Portugal",
+            "Portofino",
+            "Monaco",
+            "Ukraine",
+            "England",
+            "Australia",
+            "Japan"
+        ];
+        $(".search-field").autocomplete({
+            source: function(request, response) {
+                var results = $.ui.autocomplete.filter(destinationTags, request.term);
+                response(results.slice(0, 5));
+            }
+        });
+    });
+
+
     $('.more-desti-btn').on('click', function () {
         $('.destinations .more-destinations:first').removeClass('more-destinations');
         if (!$('.destinations .row').hasClass('more-destinations')) {
